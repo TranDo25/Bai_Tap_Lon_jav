b8e3a181fb20c2cfd120e264e4643c80ea71a94e
unknown <dotrantb25@gmail.com>
2021-10-03 21:16:05 +0700

    đẩy code backend lên lần đầu tiên, front-end tạo 1 branch khác để đẩy code nhé

---
+++ OnlineShop2/Areas/Areas/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop2.Areas.Areas.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Areas/Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(LoginModel)
+    }
+}
---
+++ OnlineShop2/Areas/Areas/Models/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop2.Areas.Areas.Models
+{
+    public class LoginModel
+    {
+    }
+}
